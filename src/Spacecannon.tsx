e39497b31ffc6856e8789df89cf3ba87daf78fda
Wiktor Łoboda <wiklob@Wiktors-MacBook-Air.local>
2025-10-26 17:14:21 +0100

    spacecannon - aim system working

--- src/Spacecannon.tsx
+++ src/Spacecannon.tsx
@@ -1,7 +1,50 @@
-function Spacecannon () {
+import { useState, useEffect } from 'react';
+
+function useMousePosition() {
+    const [position, setPosition] = useState({x: 0, y:0});
+
+    useEffect(() => {
+        const updatePosition = (e: MouseEvent) => {
+            setPosition({ x: e.clientX, y: e.clientY});
+        };
+        window.addEventListener('mousemove', updatePosition)
+    }, [])
+    
+    return position
+}
+
+function Spacecannon() {
+    const position = useMousePosition();
     return (
-        <a> def abc </a>
-    )
+        <div>
+            <div className="spacecannon-aim" style={{
+            left: position.x,
+            top: position.y
+            }}
+            />
+            <div className="spacecannon-aim-north" style={{
+            left: position.x,
+            top: position.y
+            }}
+            />
+            <div className="spacecannon-aim-south" style={{
+            left: position.x,
+            top: position.y
+            }}
+            />
+            <div className="spacecannon-aim-east" style={{
+            left: position.x,
+            top: position.y
+            }}
+            />
+            <div className="spacecannon-aim-west" style={{
+            left: position.x,
+            top: position.y
+            }}
+            />
+        </div>
+    );
+    
 }
 
 export default Spacecannon
--- src/components/HomeNewHero.tsx
+++ src/components/HomeNewHero.tsx
@@ -5,5 +5,4 @@ function HomeNewHero() {
         </div>
     )
 }
-
 export default HomeNewHero
