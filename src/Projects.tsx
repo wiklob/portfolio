e2017251656ea1db9a7df0473dd40a128cdfe40e
Wiktor Łoboda <wiklob@Wiktors-MacBook-Air.local>
2025-10-28 10:54:10 +0100

    projects tab - u

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,9 +1,24 @@
 function Projects() {
     return (
-        <div>
-            <h1> projects</h1>
-            <h2> projects</h2>
+        <div className="projects"> 
+            <div className="project-banner">
+                <div className="project-banner-image"> 
+                    <img className="project-banner-image-img" src="/u_icon.png" alt="Signature"/>
+                </div>
+                <div className="project-banner-description"> 
+                    <div className="project-banner-description-title">
+                        u 
+                    </div>
+                    <div className="project-banner-description-subtitle">
+                        The modern personal tutoring platform
+                    </div>
+                    <div className="project-banner-description-text">
+                        <b> uth <i>/youth/</i> </b> - a period of life, when creativity and courage go together, and yearn for more experience. Driven by a belief, that one is young as long as he is willing to learn, <b> Uth enables just that: tutors create their classes, and corporate clients schedule tutoring for their teams easily. </b> In the bloom of the AI age, thousands of corporate professionals require upskilling in basic use of AI-powered tools, and there is <b> no one better to teach it, than digitally-native tutors - students. </b> Uth intends to disrupt the traditional courses/training industry, which set up humongous barriers of entry while charging thousands of dollars for obsolete diplomas. <b> For a better, intergenerational workplace.</b>
+                    </div>
+                </div>
+            </div>
         </div>
+        
         
     )
 }
