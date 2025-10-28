c02d1f46a37bc0911b91d787370024ea0e06b4d1
Wiktor Łoboda <wiklob@re-byods-146-50-241-32.wireless.uva.nl>
2025-10-28 13:10:22 +0100

    njkwen

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,73 +1,86 @@
 function Projects() {
     return (
         <div className="projects"> 
-            <div className="project-banner">
-                <div className="project-banner-image"> 
-                    <img className="project-banner-image-img" src="/networker_icon.png" alt="Signature"/>
-                </div>
-                <div className="project-banner-description"> 
-                    <div className="project-banner-description-title">
-                        Networker
-                    </div>
-                    <div className="project-banner-description-subtitle">
-                        Professional Event Aggregator Browser
-                    </div>
-                    <div className="project-banner-description-text">
-                        oijsewnrfijwenkrjfn
-                    </div>
-                </div>
+            <div className="project-icon-row">
+                <img className="project-image-img" src="/networker_icon.png" alt="Signature"/>
+                <img className="project-image-img" src="/lockin_icon.png" alt="Signature"/>
+                <img className="project-image-img" src="/u_icon.png" alt="Signature"/>
             </div>
-            <div className="project-banner">
-                <div className="project-banner-image"> 
-                    <img className="project-banner-image-img" src="/lockin_icon.png" alt="Signature"/>
-                </div>
-                <div className="project-banner-description"> 
-                    <div className="project-banner-description-title">
-                        <a className="link-styling-remove" href="https://lockinstudy.com" target="_blank" rel="noopener noreferer"> Lockin </a>
+            <div className="projects-contents">
+                <div className="project-banner">
+                    <div className="project-banner-image"> 
+                        <img className="project-banner-image-img" src="/networker_icon.png" alt="Signature"/>
                     </div>
-                    <div className="project-banner-description-subtitle">
-                        Comprehensive AI-powered studying platform
-                    </div>
-                    <div className="project-banner-description-tag-list">
-                        <div className="project-banner-description-tag">
-                            <a className="link-styling-remove" href="https://lockinstudy.com" target="_blank" rel="noopener noreferer"> lockinstudy.com </a>
+                    <div className="project-banner-description"> 
+                        <div className="project-banner-description-title">
+                            <a className="link-styling-remove" href="https://lockinstudy.com" target="_blank" rel="noopener noreferer"> Networker </a>
                         </div>
-                        <div className="project-banner-description-tag-orange">
-                            launching soon
+                        <div className="project-banner-description-subtitle">
+                            Professional Event Aggregator Browser
                         </div>
-                    </div>
-                    <div className="project-banner-description-text">
-                        A comprehensive study app, enabling accessible course content consumption and exam preparations. Students can pick between uploading their own content, sourcing it from the existing database, or buying premium content from other users. The content is then processed and appropriated to different studying methods, and utilises both gamified/focus modes to make studying approachable.
-                    </div>
-                </div>
-            </div>
-            <div className="project-banner">
-                <div className="project-banner-image"> 
-                    <img className="project-banner-image-img" src="/u_icon.png" alt="Signature"/>
-                </div>
-                <div className="project-banner-description"> 
-                    <div className="project-banner-description-title">
-                        <a className="link-styling-remove" href="https://uth.academy" target="_blank" rel="noopener noreferer"> u </a>
-                    </div>
-                    <div className="project-banner-description-subtitle">
-                        The modern personal tutoring platform
-                    </div>
-                    <div className="project-banner-description-tag-list">
-                        <div className="project-banner-description-tag">
-                            <a className="link-styling-remove" href="https://uth.academy" target="_blank" rel="noopener noreferer"> uth.academy </a>
+                        <div className="project-banner-description-tag-list">
+                            <div className="project-banner-description-tag">
+                                <a className="link-styling-remove" href="https://thenetworker.agency" target="_blank" rel="noopener noreferer"> thenetworker.agency </a>
+                            </div>
+                            <div className="project-banner-description-tag-orange">
+                                MVP is up, build in progress
+                            </div>
                         </div>
-                        <div className="project-banner-description-tag-blue">
-                            MVP-in-dev, testing soon
+                        <div className="project-banner-description-text">
+                            oijsewnrfijwenkrjfn
                         </div>
                     </div>
-                    <div className="project-banner-description-text">
-                        <b> uth <i>/youth/</i> </b> - a period of life, when creativity and courage go together, and yearn for more experience. Driven by a belief, that one is young as long as he is willing to learn, <b> Uth enables just that: tutors create their classes, and corporate clients schedule tutoring for their teams easily. </b> In the bloom of the AI age, thousands of corporate professionals require upskilling in basic use of AI-powered tools, and there is <b> no one better to teach it, than digitally-native tutors - students. </b> Uth intends to disrupt the traditional courses/training industry, which set up humongous barriers of entry while charging thousands of dollars for obsolete diplomas. <b> For a better, intergenerational workplace.</b>
+                </div>
+                <div className="project-banner">
+                    <div className="project-banner-image"> 
+                        <img className="project-banner-image-img" src="/lockin_icon.png" alt="Signature"/>
+                    </div>
+                    <div className="project-banner-description"> 
+                        <div className="project-banner-description-title">
+                            <a className="link-styling-remove" href="https://lockinstudy.com" target="_blank" rel="noopener noreferer"> Lockin </a>
+                        </div>
+                        <div className="project-banner-description-subtitle">
+                            Comprehensive AI-powered studying platform
+                        </div>
+                        <div className="project-banner-description-tag-list">
+                            <div className="project-banner-description-tag">
+                                <a className="link-styling-remove" href="https://lockinstudy.com" target="_blank" rel="noopener noreferer"> lockinstudy.com </a>
+                            </div>
+                            <div className="project-banner-description-tag-orange">
+                                launching soon
+                            </div>
+                        </div>
+                        <div className="project-banner-description-text">
+                            A comprehensive study app, enabling accessible course content consumption and exam preparations. Students can pick between uploading their own content, sourcing it from the existing database, or buying premium content from other users. The content is then processed and appropriated to different studying methods, and utilises both gamified/focus modes to make studying approachable.
+                        </div>
                     </div>
                 </div>
+                <div className="project-banner">
+                    <div className="project-banner-image"> 
+                        <img className="project-banner-image-img" src="/u_icon.png" alt="Signature"/>
+                    </div>
+                    <div className="project-banner-description"> 
+                        <div className="project-banner-description-title">
+                            <a className="link-styling-remove" href="https://uth.academy" target="_blank" rel="noopener noreferer"> u </a>
+                        </div>
+                        <div className="project-banner-description-subtitle">
+                            The modern personal tutoring platform
+                        </div>
+                        <div className="project-banner-description-tag-list">
+                            <div className="project-banner-description-tag">
+                                <a className="link-styling-remove" href="https://uth.academy" target="_blank" rel="noopener noreferer"> uth.academy </a>
+                            </div>
+                            <div className="project-banner-description-tag-blue">
+                                MVP-in-dev, testing soon
+                            </div>
+                        </div>
+                        <div className="project-banner-description-text">
+                            <b> uth <i>/youth/</i> </b> - a period of life, when creativity and courage go together, and yearn for more experience. Driven by a belief, that one is young as long as he is willing to learn, <b> Uth enables just that: tutors create their classes, and corporate clients schedule tutoring for their teams easily. </b> In the bloom of the AI age, thousands of corporate professionals require upskilling in basic use of AI-powered tools, and there is <b> no one better to teach it, than digitally-native tutors - students. </b> Uth intends to disrupt the traditional courses/training industry, which set up humongous barriers of entry while charging thousands of dollars for obsolete diplomas. <b> For a better, intergenerational workplace.</b>
+                        </div>
+                    </div>
+                </div> 
             </div>
         </div>
-        
-        
     )
 }
 export default Projects
